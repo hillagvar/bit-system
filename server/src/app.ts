a7e0b0a48c2609fbf7758bb479c3300fd76c2f27
Hila Gvar <hilla.gvar@gmail.com>
2024-06-28 02:42:21 +0200

    files upd

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,6 +5,7 @@ import { coursesRouter } from "./routes/courses.router";
 import { groupsRouter } from "./routes/groups.router";
 import { studentRouter } from "./routes/student.router";
 import { lecturesRouter } from "./routes/lectures.router";
+import path from "path";
 
 const app: Application = express();
 
@@ -13,6 +14,8 @@ const app: Application = express();
 app.use(express.json());
 
 app.use(corsHeaders);
+
+app.use("/files", express.static(path.join("./files") ));
 
 app.use("/auth", authRouter);
 app.use("/courses", coursesRouter);
